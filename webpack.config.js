b15667edfb356034c121c1debf8755bfd4149d5b
Mariia Korzun <maria.a.korzun@gmail.com>
2021-04-12 19:34:14 +0300

    project with webpack is configured

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env, argv) => {
+
+    const isProd = argv.mode === 'production'
+
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+        ]
+    }
+
+    return {
+        mode: "development",
+        devServer: {
+            open: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Carousel React App',
+                template: 'public/index.html'
+            }),
+            new MiniCssExtractPlugin()
+        ]
+    }
+}
