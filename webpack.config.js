b52c20b9ab687901ef0faf8a43c6cc27aee35c95
Mariia Korzun <maria.a.korzun@gmail.com>
2021-04-27 23:28:11 +0300

    Configured eslint to ignore webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,90 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
 
-    const isProd = argv.mode === 'production'
-
-    const getStyleLoaders = () => {
-        return [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader', {
-                loader: "postcss-loader",
-                options: {
-                    postcssOptions: {
-                        plugins: [
-                            "autoprefixer"
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-    return {
-        output: {
-            path: path.resolve(__dirname, 'docs'),
-            filename: 'index.js',
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : "style-loader",
+      "css-loader",
+      {
+        loader: "postcss-loader",
+        options: {
+          postcssOptions: {
+            plugins: ["autoprefixer"],
+          },
         },
-        devtool: 'source-map',
+      },
+    ];
+  };
+  return {
+    output: {
+      path: path.resolve(__dirname, "docs"),
+      filename: "index.js",
+    },
+    devtool: "source-map",
 
-        mode: "development",
-        devServer: {
-            open: true,
-            host: '0.0.0.0'
+    mode: "development",
+    devServer: {
+      open: true,
+      host: "0.0.0.0",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
         },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                },
-                {
-                    test: /\.(css)$/,
-                    use: getStyleLoaders()
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif|ico)$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'images',
-                            name: '[name]-[sha1:hash:7].[ext]'
-                        }
-
-                    }]
-                },
-                {
-                    test: /\.svg$/,
-                    use: [
-                        {
-                            loader: 'svg-url-loader',
-                            options: {
-                                limit: 10000,
-                            },
-                        },
-                    ]
-                },
-                {
-                    test: /\.(mov|mp4)$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'video',
-                            name: '[name]-[sha1:hash:7].[ext]'
-                        }
-
-                    }]
-                }
-            ]
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders(),
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title: 'Carousel React App',
-                template: 'public/index.html'
-            }),
-            new MiniCssExtractPlugin()
-        ]
-    }
-}
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "images",
+                name: "[name]-[sha1:hash:7].[ext]",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "svg-url-loader",
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(mov|mp4)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "video",
+                name: "[name]-[sha1:hash:7].[ext]",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Carousel React App",
+        template: "public/index.html",
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+  };
+};
